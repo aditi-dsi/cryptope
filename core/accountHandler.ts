40ffbbcb0a5f42c050b465aabc8c2d28bcf1c3ed
Aditi Bindal <aditi@Aditis-MacBook-Pro.local>
2025-03-01 20:34:00 +0530

    prepare and build swap

--- core/accountHandler.ts
+++ core/accountHandler.ts
@@ -1,18 +1,27 @@
 import { PublicKey, Connection, Keypair, VersionedTransaction } from '@solana/web3.js';
 import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { Wallet } from '@coral-xyz/anchor';
+import bs58 from 'bs58';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-export const customerAccountAddress = "" // will be dynamic
+const privateKeyArray = bs58.decode(process.env.PRIVATE_KEY || '') // will be dynamic
+
+export const customerAccountAddress = new Wallet(Keypair.fromSecretKey(new Uint8Array(privateKeyArray)));
+
+console.log("customerAcc:",customerAccountAddress.publicKey.toBase58())
+
 
 export const USDC_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'); // USDC mint address
 
-export const merchantAccount = new PublicKey('ExdRLrQNBEGkJfX3r1cmP2pW8iWsYzrV1ZPytiYoVNkW'); // will be dynamic
+export const merchantAccount = new PublicKey('6uvigyE35Tmnmcvy5sgKzU4QDZcFK3ewCC3war8sLePi'); // will be dynamic
 
 console.log("USDC_MINT:", USDC_MINT.toBase58());
 console.log("merchantAccount:", merchantAccount.toBase58());
 
 // Get the associated token account for the merchant wallet
-const merchantUSDCTokenAccount = await getAssociatedTokenAddress(
+export const merchantUSDCTokenAccount = await getAssociatedTokenAddress(
     USDC_MINT,
     merchantAccount,
     true,
---
+++ core/prepareSwap.ts
@@ -0,0 +1,48 @@
+import { customerAccountAddress, merchantUSDCTokenAccount } from './accountHandler'
+
+export const prepareSwap = async () => {
+
+    const inputMint = "So11111111111111111111111111111111111111112" // will be dynamic
+    const outputMint = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" // USDC Mint Address
+    const amount = 1 * 1e6 // will be dynamic
+
+
+    /* Fetch Quote */
+
+    const getSwapQuote = await (
+        await fetch(
+            `https://api.jup.ag/swap/v1/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=50&restrictIntermediateTokens=true&swapMode=ExactOut`
+        )
+      ).json();
+      
+    console.log(JSON.stringify(getSwapQuote, null, 2));
+
+
+    /* Prepare Swap on latest quote */
+
+    const swapResponse = await (
+        await fetch('https://api.jup.ag/swap/v1/swap', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                quoteResponse: getSwapQuote,
+                userPublicKey: customerAccountAddress.publicKey.toString(), //will be dynamic
+                destinationTokenAccount: merchantUSDCTokenAccount.toString(),
+                dynamicComputeUnitLimit: true,
+                dynamicSlippage: true,
+                prioritizationFeeLamports: {
+                priorityLevelWithMaxLamports: {
+                maxLamports: 1000000,
+                priorityLevel: "veryHigh"
+                    }
+                }
+            })
+        })
+    ).json();
+    
+    console.log(swapResponse);
+}
+
+prepareSwap()
